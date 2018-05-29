ab6819cf270698c91ced3edc9eca7a1e9a6bb314
AZere <aseshgiri@gmail.com>
2018-05-29 13:45:14 -0500

    for loop a.k.a iterations

---
+++ Iterations/Iterations/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int a = 1; a < 10; a++)
+            {
+                if (a % 2 == 0)
+                {
+                    Console.WriteLine(a);
+                }
+            }
+
+            for (int b = 10; b >= 1; b--)
+            {
+                if (b % 2 == 0)
+                {
+                    Console.WriteLine(b);
+                }
+            }
+        }
+    }
+}
